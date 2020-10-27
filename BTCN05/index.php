08e8dd15148ca1c8506f3ffa336ba76e7a732f71
To Ngo <tongo0209@gmail.com>
2020-10-27 17:30:35 +0700

    tongo

---
+++ BTCN05/index.php
@@ -0,0 +1,33 @@
+<?php
+    require_once 'init.php';
+    require_once 'functions.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+<h1>CỘNG 2 SỐ</h1>
+<form action="sum.php" method="GET">
+  <div class="form-group"> 
+  <label for="number1">Số thứ nhất</label>   
+    <input type="number1" class="form-control" id="number1" name="number1">
+  </div>
+  <div class="form-group">
+  <label for="number2">Số thứ hai</label>
+    <input type="number2" class="form-control" id="number2" name="number2">
+  </div>
+  <button type="submit" class="btn btn-primary">Gửi dữ liệu</button>
+</form> 
+</div>
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</html>
